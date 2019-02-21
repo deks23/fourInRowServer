75d70ed35d65efff0652959cb6d97687171fb52a
deks23 <deks232@gmail.com>
2019-02-22 00:25:41 +0100

    initial commit

---
+++ src/main/java/pl/damiankotynia/fourinrow/server/Main.java
@@ -0,0 +1,10 @@
+package pl.damiankotynia.fourinrow.server;
+
+import pl.damiankotynia.fourinrow.model.Request;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("ads");
+        new Request();
+    }
+}
